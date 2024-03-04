a70985d48f79f4593af2af5f44fe38b1ce5e3eb5
Vittorio Aiello <vittorio.aiello@fht.org>
2024-03-04 18:27:24 +0100

    Update setup.py
    Addede reqs

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='labguruApi', version='0.1', packages=find_packages())
+setup(name='labguruApi', version='0.1', packages=find_packages(), install_requires=['requests', 'smtplib', 'pandas'])
